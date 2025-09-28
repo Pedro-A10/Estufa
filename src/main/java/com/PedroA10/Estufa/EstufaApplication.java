9f974b312f377cda83f9636d6ffbe32703f14d9f
Pedro A. <pedroaugusto732@gmail.com>
2025-09-28 09:02:34 -0300

    Adição do projeto ao repositório.

---
+++ src/main/java/com/PedroA10/Estufa/EstufaApplication.java
@@ -0,0 +1,13 @@
+package com.PedroA10.Estufa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstufaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstufaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/PedroA10/Estufa/EstufaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PedroA10.Estufa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstufaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
